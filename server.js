0c1f91ab8d69a2748998591ae6c336a57c92a45b
OG PRATES <141651842+OGp1@users.noreply.github.com>
2024-11-23 22:23:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const veiculosRotas = require('./routes/veiculos');
 
 // definição de parâmetros do servidor
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Esta é a raiz do servidor.')
